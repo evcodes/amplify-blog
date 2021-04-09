f42e29401b0c7bc44be5c579a58f2a4a63c35f58
Eddy <eddvar@amazon.com>
2021-04-09 11:51:32 -0400

    changing app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Testing Github deployment</h1>
+      <h1>Testing Github deployment again</h1>
     </div>
   );
 }
